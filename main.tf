295e06ef5cf5fbbafb0d720abe5d56de63a351ed
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-03 13:39:09 +0545

    feat: Provisioning for Azure Resource groups and Azure Container Registry

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+        required_providers {
+                azurerm = {
+                        source = "hashicorp/azurerm"
+                        version = "=4.1.0"
+                }
+        }
+}
+
+provider "azurerm" {
+        features {}
+}
+
+resource "azurerm_rg_june_2026" "rg_june_2026" {
+        name = "rg_pipeline_project"
+        location = "southeastasia" # I am only allowed in 5 regions because of Students' restrictions. So, expect some latency
+}
+
+resource "azurerm_container_registry" "acr" {
+        name = "acr_prince_pipeline_project_june_2026"
+        resource_group_name = "azurerm_rg_june_2026.rg_june_2026.name
+        location = azurerm_rg_june_2026.rg_june_2026.location
+        sku = "Basic"
+        admin_enabled = true
+}
