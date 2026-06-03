d01f61ede4947dd8b7a0dc5efbdd7237842d4aa3
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-03 20:32:21 +0545

    feat: Add Log Analytics Workspace resource, Container App Enviornment and Container App resource and create template for application container

--- main.tf
+++ main.tf
@@ -24,3 +24,69 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
+
+resource "azurerm_log_analytics_workspace" "law_rg_june_2026" {
+  name                = "log-analytics-workspace-for-rg-june-2026"
+  location            = azurerm_resource_group.rg_june_2026.location
+  resource_group_name = azurerm_resource_group.rg_june_2026.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "azcontappenv" {
+  name                       = "env-serve-and-log-app"
+  location                   = azurerm_resource_group.rg_june_2026.location
+  resource_group_name        = azurerm_resource_group.rg_june_2026.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law_rg_june_2026.id
+}
+# This thing above is the serverless cluster context
+
+resource "azurerm_container_app" "azapp" {
+  name                         = "serve-and-log-cont-app"
+  container_app_environment_id = azurerm_container_app_environment.azcontappenv.id
+  resource_group_name          = azurerm_resource_group.rg_june_2026.name
+  revision_mode                = "Single"
+  # Close old container and create a new one when update happens, saves money for me
+
+  lifecycle {
+    ignore_changes = [
+      template[0].container[0].image
+      # Because template is nested as list of objects. Which is valid since this is how Kubernetes deploy.yaml workds
+    ]
+  }
+
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.acr.admin_password
+  }
+
+  registry {
+    server               = azurerm_container_registry.acr.login_server
+    username             = azurerm_container_registry.acr.admin_username
+    password_secret_name = "acr-password"
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 8080
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  template {
+    container {
+      name  = "june2026fastapiapplication"
+      image = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+      # I keep this above image because we haven't deployed yet and hence ACR is empty
+      cpu    = "0.25"
+      memory = "0.5Gi"
+    }
+  }
+}
+
+output "june2026fastapiapplication_url" {
+  value = azurerm_container_app.azapp.latest_revision_fqdn
+}
