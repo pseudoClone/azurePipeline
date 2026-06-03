e0719443f8fbe7a75e852329231cabda37c1de29
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-03 13:49:08 +0545

    style: Auto terraform format using `terraform fmt`

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 terraform {
-        required_providers {
-                azurerm = {
-                        source = "hashicorp/azurerm"
-                        version = "=4.1.0"
-                }
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
 }
 
 provider "azurerm" {
-        features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "rg_june_2026" {
-        name = "rg_pipeline_project"
-        location = "southeastasia" # I am only allowed in 5 regions because of Students' restrictions. So, expect some latency
+  name     = "rg_pipeline_project"
+  location = "southeastasia" # I am only allowed in 5 regions because of Students' restrictions. So, expect some latency
 }
 
 resource "azurerm_container_registry" "acr" {
-        name = "acrprincepipelinejune2026"
-        resource_group_name = azurerm_resource_group.rg_june_2026.name
-        location = azurerm_resource_group.rg_june_2026.location
-        sku = "Basic"
-        admin_enabled = true
+  name                = "acrprincepipelinejune2026"
+  resource_group_name = azurerm_resource_group.rg_june_2026.name
+  location            = azurerm_resource_group.rg_june_2026.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
